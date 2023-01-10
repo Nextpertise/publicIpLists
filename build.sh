668a681a8c8a22f4baa61a4870c0194938cee2c9
Michael <michael@nextpertise.nl>
2023-01-10 16:57:49 +0100

    Implement kubernetes pipeline

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t nextpertise/public-ip-lists:latest .
-docker rm -f public-ip-lists; sleep 1; docker run -p 5000:5000 --name public-ip-lists -d nextpertise/public-ip-lists
+docker build -t registry.nextpertise.tools/nextpertise/public-ip-lists:latest .
+docker rm -f public-ip-lists; sleep 1; docker run -p 5000:5000 --name public-ip-lists -d registry.nextpertise.tools/nextpertise/public-ip-lists
